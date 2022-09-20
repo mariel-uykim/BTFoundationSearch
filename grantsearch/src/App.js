a55fed1c92843218638dc74cc23c64d33c69cd95
Mariel Uykim <m@Mariels-MacBook-Air.local>
2022-09-20 11:45:52 +0800

    results grid layout + link to webpage

--- grantsearch/src/App.js
+++ grantsearch/src/App.js
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <SearchBox/>
       <div className='resultBoxes'>
-        <DisplayBox name = 'Grant 1' desc = 'something1'/>
-        <DisplayBox name = 'Grant 2' desc = 'something2'/>
+        <DisplayBox name = 'Grant 1' desc = 'something1' image='https://logos-world.net/wp-content/uploads/2021/04/Westpac-Banking-Corporation-Logo-2003-present.png'/>
+        <DisplayBox name = 'Grant 2' desc = 'something2' siteURL='https://www.google.com/'/>
         <DisplayBox name = 'Grant 3' desc = 'something3'/>
+        <DisplayBox name = 'Grant 4' desc = 'something4' image='https://pbs.twimg.com/profile_images/1205289863924174848/90emyq50_400x400.png'/>
       </div>
     </div>
   );
--- grantsearch/src/components/DisplayBox.js
+++ grantsearch/src/components/DisplayBox.js
@@ -3,13 +3,18 @@ import '../style/App.css'
 import { Card, CardActions, CardContent, CardMedia, Button, Typography }from '@mui/material'
 
 const DisplayBox = (props) => {
+  const openLink = url => {
+    window.open(url, '_blank', 'noopener,noreferrer')
+  }
   return (
     <Card sx={{ maxWidth: 345 }} className='displayBox'>
       <CardMedia
         component="img"
         alt="company logo"
         height="140"
-        image="http://atlas-content-cdn.pixelsquid.com/stock-images/animal-egg-x7O64d1-600.jpg"
+        width="160"
+        image= {props.image ? props.image
+        : "http://atlas-content-cdn.pixelsquid.com/stock-images/animal-egg-x7O64d1-600.jpg"}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
@@ -21,7 +26,7 @@ const DisplayBox = (props) => {
       </CardContent>
       <CardActions>
         <Button size="small">Bookmark</Button>
-        <Button size="small">Open Webpage</Button>
+        <Button size="small" onClick={() => openLink(props.siteURL)}>Open Webpage</Button>
       </CardActions>
     </Card>
   )
